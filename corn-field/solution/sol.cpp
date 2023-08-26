86eef884d2ebd2b3c076013ea9d7e9ec3de04488
sion-k <ygonepiece@naver.com>
2023-08-27 00:41:57 +0900

    옥수수밭

--- corn-field/solution/sol.cpp
+++ corn-field/solution/sol.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-const int dydx[4][2]{{1,0},{0,1},{-1,0},{0,-1}};
+const int dydx[4][2]{ {1,0},{0,1},{-1,0},{0,-1} };
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -18,7 +18,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> choco[i][j];
-            rc[choco[i][j]] = {i, j};
+            rc[choco[i][j]] = { i, j };
         }
     }
 
@@ -43,7 +43,7 @@ int main() {
         int x = pq.top();
         pq.pop();
         cout << rc[x].first + 1 << " " << rc[x].second + 1 << "\n";
-        for (auto u: dydx) {
+        for (auto u : dydx) {
             int nr = rc[x].first + u[0], nc = rc[x].second + u[1];
             if (nr >= 0 && nr < n && nc >= 0 && nc < m && !eat[nr][nc]) {
                 eat[nr][nc] = true;
