f635b791de734e41af8485fa6b3c841817317429
sion-k <ygonepiece@naver.com>
2023-09-15 20:11:59 +0900

    데이터 30개로 축소

--- present/generator/generator.cpp
+++ present/generator/generator.cpp
@@ -7,13 +7,6 @@ const int N_MIN = 1, N_MAX = 5'000;
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
 
-    int n;
-
-    if (argc >= 3 && atoi(argv[1]) == 1) {
-        n = atoi(argv[2]);
-    } else {
-        n = rnd.next(N_MIN, N_MAX);
-    }
-
-    cout << n << "\n";
+    int n = opt<int>("n");
+    cout << n << '\n';
 }
